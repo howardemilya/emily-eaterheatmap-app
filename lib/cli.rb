f82e6e98af6eb5afdeaee028cc0d2602bae8d659
Emily Howard <howard.emilya@gmail.com>
2017-12-08 01:48:07 +0000

    updated CLI to include greeting, pause, and list available cities

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,5 +5,24 @@ require 'nokogiri'
 
 class CommandLineInteface
 
+  def run
+    greet_user
+    sleep(3)
+    list_cities
+  end
+
+  CITY_WEBSITE = "https://www.eater.com/cities-directory"
+
+  def greet_user
+    puts "Welcome to your local restaurant hot list!"
+    sleep(3)
+    puts "Please enter the number for the city you are interested in:"
+  end
+
+  def list_cities
+    Scraper.scrape_cities_page(CITY_WEBSITE)
+  end
+
+
 
 end
